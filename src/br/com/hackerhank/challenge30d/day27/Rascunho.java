71561dfc241e267d3bd2dac61e13b1c8f6bdd101
wemmelotec <tonmelo@hotmail.com>
2021-06-11 14:49:28 -0300

    done day 27

---
+++ src/br/com/hackerhank/challenge30d/day27/Rascunho.java
@@ -0,0 +1,11 @@
+package br.com.hackerhank.challenge30d.day27;
+
+public class Rascunho {
+    public static void main(String[] args) {
+        String first = "Teste123";
+        System.out.println(first);
+        String second = "Teste" + 123;
+        System.out.println(second);
+        System.out.println(first.equals(second));
+    }
+}
