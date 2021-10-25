cc8737b899e26e19d316833e0e2dbbd67f2feb1a
wemmelotec <tonmelo@hotmail.com>
2021-10-25 14:51:33 -0300

    challenge End-of-file

---
+++ src/br/com/hackerhank/challenges/Challenge9.java
@@ -0,0 +1,17 @@
+package br.com.hackerhank.challenges;
+
+import java.util.Scanner;
+
+public class Challenge9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numeroDaLinha = 0;
+
+        while (input.hasNext()){
+            numeroDaLinha++;
+            System.out.println(numeroDaLinha+ " " + input.nextLine());
+        }
+
+
+    }
+}
