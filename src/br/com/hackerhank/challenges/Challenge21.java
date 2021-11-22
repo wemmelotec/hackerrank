9a84262a350dcdb9ce63839c639de7252f4f5afe
wemmelotec <tonmelo@hotmail.com>
2021-11-22 13:52:41 -0300

    Challenge21

---
+++ src/br/com/hackerhank/challenges/Challenge21.java
@@ -0,0 +1,18 @@
+package br.com.hackerhank.challenges;
+
+import java.util.Scanner;
+//Java Regex
+public class Challenge21 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+class MyRegex{
+    String ip0to255 = "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])";
+
+    public String pattern = ip0to255 + "\\." + ip0to255 + "\\." + ip0to255 + "\\." + ip0to255;
+}
